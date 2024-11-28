def92a7ff93f897aca2c1298df77ce9a116aad6e
richard-tang199 <tqd18907@nudt.edu.cn>
2024-11-28 09:02:02 +0800

    upload files

--- runners/run_asd_pollute.sh
+++ runners/run_asd_pollute.sh
@@ -2,11 +2,19 @@
 
 groups=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12")
 ratios=("0.02" "0.05" "0.1" "0.15")
-ratios2=("1" "2" "3" "4")
 
 for group in "${groups[@]}"
 do
+  # realistic anomaly pollution case
+  python main.py --model_name PatchContrast --mode realistic --anomaly_ratio 0.02 --num_epochs 101 --data_name "ASD" --group "$group" --remove_anomaly 1
+  python train_other_model.py --model_name mtad_gat --mode realistic --anomaly_ratio 0.02 --num_epochs 50 --data_name "ASD" --group "$group"
+  python train_other_model.py --model_name MAUT --mode realistic --anomaly_ratio 0.02 --num_epochs 100 --data_name "ASD" --group "$group"
+  python train_other_model.py --model_name usad --mode realistic --anomaly_ratio 0.02 --num_epochs 100 --data_name "ASD" --group "$group"
+  python train_other_model.py --model_name cad --mode realistic --anomaly_ratio 0.02 --num_epochs 30 --data_name "ASD" --group "$group"
+  python train_other_model.py --model_name NormFAAE --mode realistic --anomaly_ratio 0.02 --num_epochs 300 --data_name "ASD" --group "$group"
+  python train_other_model.py --model_name FGANomaly --mode realistic --anomaly_ratio 0.02 --num_epochs 100 --data_name "ASD" --group "$group"
   for ratio in "${ratios[@]}"
+  # simulated anomaly pollution case
   do
     python main.py --model_name PatchContrast --mode simulated --anomaly_ratio "$ratio" --num_epochs 101 --data_name "ASD" --group "$group" --remove_anomaly 1
     python train_other_model.py --model_name mtad_gat --mode simulated --anomaly_ratio "$ratio" --num_epochs 50 --data_name "ASD" --group "$group"
@@ -15,18 +23,6 @@ do
     python train_other_model.py --model_name cad --mode simulated --anomaly_ratio "$ratio" --num_epochs 30 --data_name "ASD" --group "$group"
     python train_other_model.py --model_name NormFAAE --mode simulated --anomaly_ratio "$ratio" --num_epochs 300 --data_name "ASD" --group "$group"
     python train_other_model.py --model_name FGANomaly --mode simulated --anomaly_ratio "$ratio" --num_epochs 100 --data_name "ASD" --group "$group"
-    python train_other_model.py --model_name KMeans --mode simulated --anomaly_ratio "$ratio" --num_epochs 100 --data_name "ASD" --group "$group"
-  done
-  for ratio in "${ratios2[@]}"
-  do
-    python main.py --model_name PatchContrast --mode realistic --window_length 1024 --anomaly_ratio "$ratio" --num_epochs 101 --data_name ASD --group "$group" --remove_anomaly 1
-    python train_other_model.py --model_name mtad_gat --mode realistic --anomaly_ratio "$ratio" --num_epochs 50 --data_name ASD --group "$group"
-    python train_other_model.py --model_name MAUT --mode realistic --anomaly_ratio "$ratio" --num_epochs 100 --data_name ASD --group "$group"
-    python train_other_model.py --model_name usad --mode realistic --anomaly_ratio "$ratio" --num_epochs 100 --data_name ASD --group "$group"
-    python train_other_model.py --model_name cad --mode realistic --anomaly_ratio "$ratio" --num_epochs 30 --data_name ASD --group "$group"
-    python train_other_model.py --model_name NormFAAE --mode realistic --anomaly_ratio "$ratio" --num_epochs 300 --data_name ASD --group "$group"
-    python train_other_model.py --model_name FGANomaly --mode realistic --anomaly_ratio "$ratio" --num_epochs 100 --data_name ASD --group "$group"
-    python train_other_model.py --model_name KMeans --mode realistic --anomaly_ratio "$ratio" --num_epochs 100 --data_name ASD --group "$group"
   done
 done
 
--- runners/run_ucr_pollute.sh
+++ runners/run_ucr_pollute.sh
@@ -2,12 +2,17 @@
 
 groups=("006" "025" "048" "141" "145" "160" "173")
 ratios=("0.02" "0.05" "0.1" "0.15" "0.2")
-ratio2=("1" "2" "3" "4")
 
 for group in "${groups[@]}"
 do
+  python train_tsb.py --model_name SAND --mode realistic --anomaly_ratio 0.02 --data_name "UCR" --group "$group"
+  python main.py --model_name PatchContrast --mode realistic --anomaly_ratio 0.02 --num_epochs 201 --data_name "UCR" --group "$group" --remove_anomaly 0
+  python train_other_model.py --model_name DAMP --mode realistic --anomaly_ratio 0.02 --data_name "UCR" --group "$group"
+  python train_other_model.py --model_name MP --mode realistic --anomaly_ratio 0.02  --data_name "UCR" --group "$group"
+  python train_other_model.py --model_name MAUT --mode realistic --anomaly_ratio 0.02 --num_epochs 100 --data_name "UCR" --group "$group"
+  python train_other_model.py --model_name cad --mode realistic --anomaly_ratio 0.02 --num_epochs 30 --data_name "UCR" --group "$group"
+  python train_other_model.py --model_name mtad_gat --mode realistic --anomaly_ratio 0.02 --num_epochs 50 --data_name "UCR" --group "$group"
   for ratio in "${ratios[@]}"
-  # simulated
   do
     python train_tsb.py --model_name SAND --mode simulated --anomaly_ratio "$ratio" --data_name "UCR" --group "$group"
     python main.py --model_name PatchContrast --mode simulated --anomaly_ratio "$ratio" --num_epochs 201 --data_name "UCR" --group "$group" --remove_anomaly 0
@@ -16,19 +21,5 @@ do
     python train_other_model.py --model_name MAUT --mode simulated --anomaly_ratio "$ratio" --num_epochs 100 --data_name "UCR" --group "$group"
     python train_other_model.py --model_name cad --mode simulated --anomaly_ratio "$ratio" --num_epochs 30 --data_name "UCR" --group "$group"
     python train_other_model.py --model_name mtad_gat --mode simulated --anomaly_ratio "$ratio" --num_epochs 50 --data_name "UCR" --group "$group"
-    python train_other_model.py --model_name KMeans --mode simulated --anomaly_ratio "$ratio" --num_epochs 100 --data_name "UCR" --group "$group"
-  done
-  # rel
-  for ratio in "${ratios2[@]}"
-  do
-    python train_tsb.py --model_name SAND --mode realistic --anomaly_ratio "$ratio" --data_name "UCR" --group "$group"
-    python main.py --model_name PatchContrast --mode realistic --anomaly_ratio "$ratio" --num_epochs 201 --data_name "UCR" --group "$group" --remove_anomaly 0
-    python main.py --model_name PatchContrast --mode realistic --anomaly_ratio "$ratio" --num_epochs 201 --data_name "UCR" --group "$group" --remove_anomaly 1
-    python train_other_model.py --model_name DAMP --mode realistic --anomaly_ratio "$ratio" --data_name "UCR" --group "$group"
-    python train_other_model.py --model_name MP --mode realistic --anomaly_ratio "$ratio"  --data_name "UCR" --group "$group"
-    python train_other_model.py --model_name MAUT --mode realistic --anomaly_ratio "$ratio" --num_epochs 100 --data_name "UCR" --group "$group"
-    python train_other_model.py --model_name cad --mode realistic --anomaly_ratio "$ratio" --num_epochs 30 --data_name "UCR" --group "$group"
-    python train_other_model.py --model_name mtad_gat --mode realistic --anomaly_ratio "$ratio" --num_epochs 50 --data_name "UCR" --group "$group"
-    python train_other_model.py --model_name KMeans --num_epochs 100 --data_name "UCR" --group "$group" --anomaly_ratio "$ratio"
   done
 done
