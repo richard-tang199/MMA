deae4415abc80e8c66b22bb22dbe7132b0cbe0f1
richard-tang199 <tqd18907@nudt.edu.cn>
2024-11-28 09:08:53 +0800

    update files

--- runners/run_ucr_pollute.sh
+++ runners/run_ucr_pollute.sh
@@ -6,7 +6,7 @@ ratios=("0.02" "0.05" "0.1" "0.15")
 for group in "${groups[@]}"
 do
   python train_tsb.py --model_name SAND --mode realistic --anomaly_ratio 0.02 --data_name "UCR" --group "$group"
-  python main.py --model_name PatchContrast --mode realistic --anomaly_ratio 0.02 --num_epochs 201 --data_name "UCR" --group "$group" --remove_anomaly 0
+  python main.py --model_name PatchContrast --mode realistic --anomaly_ratio 0.02 --num_epochs 201 --data_name "UCR" --group "$group" --remove_anomaly 1
   python train_other_model.py --model_name DAMP --mode realistic --anomaly_ratio 0.02 --data_name "UCR" --group "$group"
   python train_other_model.py --model_name MP --mode realistic --anomaly_ratio 0.02  --data_name "UCR" --group "$group"
   python train_other_model.py --model_name MAUT --mode realistic --anomaly_ratio 0.02 --num_epochs 100 --data_name "UCR" --group "$group"
@@ -15,7 +15,7 @@ do
   for ratio in "${ratios[@]}"
   do
     python train_tsb.py --model_name SAND --mode simulated --anomaly_ratio "$ratio" --data_name "UCR" --group "$group"
-    python main.py --model_name PatchContrast --mode simulated --anomaly_ratio "$ratio" --num_epochs 201 --data_name "UCR" --group "$group" --remove_anomaly 0
+    python main.py --model_name PatchContrast --mode simulated --anomaly_ratio "$ratio" --num_epochs 201 --data_name "UCR" --group "$group" --remove_anomaly 1
     python train_other_model.py --model_name DAMP --mode simulated --anomaly_ratio "$ratio" --data_name "UCR" --group "$group"
     python train_other_model.py --model_name MP --mode simulated --anomaly_ratio "$ratio"  --data_name "UCR" --group "$group"
     python train_other_model.py --model_name MAUT --mode simulated --anomaly_ratio "$ratio" --num_epochs 100 --data_name "UCR" --group "$group"
