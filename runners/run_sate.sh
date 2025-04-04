2f3096a6cf09c0138d75db45db48e35e4aaacc1f
richard-tang199 <82788620+richard-tang199@users.noreply.github.com>
2025-04-04 14:52:57 +0800

    Delete runners/run_sate.sh

--- runners/run_sate.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-group_list=("real_satellite_data_1" "real_satellite_data_2")
-for group in "${group_list[@]}"
-do
-  python main.py --model_name PatchContrast --data_name "sate" --group "$group" --remove_anomaly 1 --window_length 1024
-done
